18193d8430c0596774e27bfe01c903551eb5322f
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 04:56:15 -0700

    Migrate Python package environment to Clause

--- modules/python/default.nix
+++ modules/python/default.nix
@@ -1,8 +1,2 @@
 { config, lib, pkgs, ... }:
-
-{
-  options.myConfig.modules.python.enable = lib.mkEnableOption "Python runtime with uv";
-  config = lib.mkIf config.myConfig.modules.python.enable {
-    environment.systemPackages = [ (pkgs.python3.withPackages (ps: with ps; [ boto3 ])) ];
-  };
-}
+{ "config" = (lib."mkIf" (config."myConfig"."modules"."python"."enable") ({ "environment" = { "systemPackages" = [ ((pkgs."python3"."withPackages" ((__clause_argument_0: [ ((__clause_argument_0)."boto3") ])))) ]; }; })); "options" = { "myConfig" = { "modules" = { "python" = { "enable" = (lib."mkEnableOption" ("Python runtime with uv")); }; }; }; }; }
