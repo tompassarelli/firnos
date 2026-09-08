535ef6c3bad9da23c907f3f6941c733653ab448e
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 23:55:22 -0700

    Migrate Remmina and Starship modules to Clause

--- modules/remmina/default.nix
+++ modules/remmina/default.nix
@@ -1,18 +1,2 @@
 { config, lib, pkgs, ... }:
-
-let
-  username = config.myConfig.modules.users.username;
-in
-{
-  options.myConfig.modules.remmina.enable = lib.mkEnableOption "Remmina remote desktop client";
-  config = lib.mkIf config.myConfig.modules.remmina.enable {
-    environment.systemPackages = with pkgs; [ remmina ];
-    home-manager.users.${username} = {
-      xdg.configFile."autostart/remmina-applet.desktop".text = ''
-        [Desktop Entry]
-        Hidden=true
-
-      '';
-    };
-  };
-}
+{ "config" = (lib."mkIf" (config."myConfig"."modules"."remmina"."enable") ({ "environment" = { "systemPackages" = [ (pkgs."remmina") ]; }; "home-manager" = { "users" = { ${config."myConfig"."modules"."users"."username"} = { "xdg" = { "configFile" = { "autostart/remmina-applet.desktop" = { "text" = "[Desktop Entry]\nHidden=true\n\n"; }; }; }; }; }; }; })); "options" = { "myConfig" = { "modules" = { "remmina" = { "enable" = (lib."mkEnableOption" ("Remmina remote desktop client")); }; }; }; }; }
