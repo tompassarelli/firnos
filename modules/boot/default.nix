4fb6ac1b6a2c7b2e5499b4fcd3fce755f9e0122e
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 23:14:23 -0700

    Migrate boot and Nix settings to Clause with inline comments

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -1,17 +1,2 @@
-{ config, lib, pkgs, ... }:
-
-{
-  options.myConfig.modules.boot.enable = lib.mkEnableOption "boot configuration";
-  config = lib.mkIf config.myConfig.modules.boot.enable {
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.kernelModules = [ "uinput" ];
-    boot.kernelParams = [ "amdgpu.gpu_recovery=1" "nmi_watchdog=panic" "amdgpu.cwsr_enable=0" ];
-    boot.kernel.sysctl = {
-      "kernel.hardlockup_panic" = 1;
-      "kernel.softlockup_panic" = 1;
-      "kernel.panic_on_oops" = 1;
-      "kernel.panic" = 20;
-    };
-  };
-}
+{ config, lib, ... }:
+{ "config" = (lib."mkIf" (config."myConfig"."modules"."boot"."enable") ({ "boot" = { "kernel" = { "sysctl" = { "kernel.hardlockup_panic" = 1; "kernel.panic" = 20; "kernel.panic_on_oops" = 1; "kernel.softlockup_panic" = 1; }; }; "kernelModules" = [ ("uinput") ]; "kernelParams" = [ ("amdgpu.gpu_recovery=1") ("nmi_watchdog=panic") ("amdgpu.cwsr_enable=0") ]; "loader" = { "efi" = { "canTouchEfiVariables" = true; }; "systemd-boot" = { "enable" = true; }; }; }; })); "options" = { "myConfig" = { "modules" = { "boot" = { "enable" = (lib."mkEnableOption" ("boot configuration")); }; }; }; }; }
