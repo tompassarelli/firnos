dea037f63828f4e58ecaa4505714b574e4542151
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 09:05:22 -0700

    Migrate pinned Babashka package module to Clause

--- modules/babashka/default.nix
+++ modules/babashka/default.nix
@@ -1,27 +1,2 @@
 { config, lib, pkgs, ... }:
-
-((version: ((babashka: {
-  options.myConfig.modules.babashka.enable = lib.mkEnableOption "Babashka native Clojure scripting";
-  config = lib.mkIf config.myConfig.modules.babashka.enable {
-    environment.systemPackages = [ babashka ];
-  };
-}) (pkgs.stdenvNoCC.mkDerivation {
-    pname = "babashka";
-    version = version;
-    src = pkgs.fetchurl {
-      url = "https://github.com/babashka/babashka/releases/download/v${version}/babashka-${version}-linux-amd64-static.tar.gz";
-      hash = "sha256-6z7dEoJ28Lb73vyxjcfUJlKpXqQJqBs08I42uKw8vDw=";
-    };
-    sourceRoot = ".";
-    dontBuild = true;
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 bb "$out/bin/bb"
-      runHook postInstall
-    '';
-    doInstallCheck = true;
-    installCheckPhase = ''
-      test "$("$out/bin/bb" --version)" = "babashka v${version}"
-    '';
-    meta = pkgs.babashka.meta;
-  }))) "1.13.220")
+{ "config" = (lib."mkIf" (config."myConfig"."modules"."babashka"."enable") ({ "environment" = { "systemPackages" = [ ((pkgs."stdenvNoCC"."mkDerivation" ({ "doInstallCheck" = true; "dontBuild" = true; "installCheckPhase" = "test \"$(\"$out/bin/bb\" --version)\" = \"babashka v1.13.220\"\n"; "installPhase" = "runHook preInstall\ninstall -Dm755 bb \"$out/bin/bb\"\nrunHook postInstall\n"; "meta" = pkgs."babashka"."meta"; "pname" = "babashka"; "sourceRoot" = "."; "src" = (pkgs."fetchurl" ({ "hash" = "sha256-6z7dEoJ28Lb73vyxjcfUJlKpXqQJqBs08I42uKw8vDw="; "url" = "https://github.com/babashka/babashka/releases/download/v1.13.220/babashka-1.13.220-linux-amd64-static.tar.gz"; })); "version" = "1.13.220"; }))) ]; }; })); "options" = { "myConfig" = { "modules" = { "babashka" = { "enable" = (lib."mkEnableOption" ("Babashka native Clojure scripting")); }; }; }; }; }
