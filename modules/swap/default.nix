5680e8c4cc492e7a9a377fa473aa27864d8aaf4a
tompassarelli <tom.passarelli@protonmail.com>
2026-09-07 22:11:03 -0700

    Migrate swap module to checked Clause source

--- modules/swap/default.nix
+++ modules/swap/default.nix
@@ -1,24 +1,2 @@
-{ config, lib, pkgs, ... }:
-
-{
-  options.myConfig.modules.swap.enable = lib.mkEnableOption "zram-based compressed swap";
-  config = lib.mkIf config.myConfig.modules.swap.enable {
-    zramSwap.enable = true;
-    zramSwap.algorithm = "zstd";
-    zramSwap.memoryPercent = 50;
-    swapDevices = [
-      {
-        device = "/swap/swapfile";
-        priority = 0;
-      }
-    ];
-    boot.resumeDevice = "/dev/mapper/cryptroot";
-    boot.kernelParams = [ "resume_offset=347227782" ];
-    boot.kernel.sysctl = {
-      "vm.swappiness" = 180;
-      "vm.watermark_boost_factor" = 0;
-      "vm.watermark_scale_factor" = 125;
-      "vm.page-cluster" = 0;
-    };
-  };
-}
+{ config, lib, ... }:
+{ "config" = (lib."mkIf" (config."myConfig"."modules"."swap"."enable") ({ "boot" = { "kernel" = { "sysctl" = { "vm.page-cluster" = 0; "vm.swappiness" = 180; "vm.watermark_boost_factor" = 0; "vm.watermark_scale_factor" = 125; }; }; "kernelParams" = [ ("resume_offset=347227782") ]; "resumeDevice" = "/dev/mapper/cryptroot"; }; "swapDevices" = [ ({ "device" = "/swap/swapfile"; "priority" = 0; }) ]; "zramSwap" = { "algorithm" = "zstd"; "enable" = true; "memoryPercent" = 50; }; })); "options" = { "myConfig" = { "modules" = { "swap" = { "enable" = (lib."mkEnableOption" ("zram-based compressed swap")); }; }; }; }; }
