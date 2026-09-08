e487ead8999c496554a57703256505fb41c3f4fd
tompassarelli <tom.passarelli@protonmail.com>
2026-09-08 03:45:01 -0700

    Migrate Mako notification module to Clause

--- modules/mako/default.nix
+++ modules/mako/default.nix
@@ -1,25 +1,2 @@
-{ config, lib, pkgs, ... }:
-
-let
-  username = config.myConfig.modules.users.username;
-in
-{
-  options.myConfig.modules.mako.enable = lib.mkEnableOption "Mako notification daemon";
-  config = lib.mkIf config.myConfig.modules.mako.enable {
-    home-manager.users.${username} = {
-      services.mako = {
-        enable = true;
-        settings = {
-          default-timeout = 0;
-          icons = 0;
-          "app-name=kitty" = {
-            default-timeout = 0;
-          };
-          "app-name=Spotify" = {
-            invisible = 1;
-          };
-        };
-      };
-    };
-  };
-}
+{ config, lib, ... }:
+{ "config" = (lib."mkIf" (config."myConfig"."modules"."mako"."enable") ({ "home-manager" = { "users" = { ${config."myConfig"."modules"."users"."username"} = { "services" = { "mako" = { "enable" = true; "settings" = { "app-name=Spotify" = { "invisible" = 1; }; "app-name=kitty" = { "default-timeout" = 0; }; "default-timeout" = 0; "icons" = 0; }; }; }; }; }; }; })); "options" = { "myConfig" = { "modules" = { "mako" = { "enable" = (lib."mkEnableOption" ("Mako notification daemon")); }; }; }; }; }
